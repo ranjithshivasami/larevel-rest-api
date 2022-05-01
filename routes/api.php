a6682bbbcfa5dcf3ef4c653d236392be537f63d5
Ranjith Sivasami <ranjithshivasami@gmail.com>
2022-05-02 00:04:52 +0530

    Pasport authendicationintegrated

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+ Route::prefix('v1')->group(function () {
+    // Route::middleware('log.route')->group(function(){
+
+    // });
+    Route::post('login', [LoginController::class, 'login']);
+    Route::post('register', [LoginController::class, 'register']);
+  });
